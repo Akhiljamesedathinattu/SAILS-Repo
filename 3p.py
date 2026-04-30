b04ae64feecacdbfad4c092edee923a79a8bef19
SAILS <sails@IBAB-Workshop-Comp18.ibab.ac.in>
2026-04-30 14:17:17 +0530

    my first file

---
+++ 3p.py
@@ -0,0 +1,11 @@
+def swap(a,b):
+    return b,a 
+def mymain():
+    a=5
+    b=10
+    new_a,new_b_=swap(a,b)
+    print(new_a,new_b_)
+        
+if __name__ == "__main__":
+    # print("this is the beginning of my program")
+    mymain()
