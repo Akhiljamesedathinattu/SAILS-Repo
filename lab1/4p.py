4fc2acf823291c6d797a85f6a0d23111d6cbaf28
SAILS <sails@IBAB-Workshop-Comp18.ibab.ac.in>
2026-05-08 10:40:36 +0530

    first lab exercise

---
+++ lab1/4p.py
@@ -0,0 +1,11 @@
+def oddeven():
+    num=int(input())
+    if num%2==0:
+        print("even")
+    if num%2==1:
+        print("odd")
+def mymain():
+    oddeven()
+if __name__ == "__main__":
+    # print("this is the beginning of my program")
+    mymain()
