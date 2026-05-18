df8f62338da6ebcde48f1d690e5ac22d44edbe75
SAILS <sails@IBAB-Workshop-Comp18.ibab.ac.in>
2026-05-18 15:31:41 +0530

    modified

--- 7_lab/matrix subtraction.py
+++ 7_lab/matrix subtraction.py
@@ -2,20 +2,31 @@ def matrix_subtraction():
     m1 =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     m2 =[[10, 20, 30], [40, 50, 60], [70, 80, 90]]
     m=[]
-    item=[]
+
+    s = len(m1)
+    print("Matrix A:")
+    for row in m1:
+        print(row)
+    print("\n matrix B:")
+    for row in m2:
+        print(row)
 
 
 
 
 
     for item1, item2 in zip(m1, m2):
-        s = len(item1)
+        item = []
         for i in range(s):
             v = item1[i] - item2[i]
             item.append(v)
+        m.append(item)
 
-    m.append(item)
-    print(m)
+    print("\nsubstraction A-B:")
+    for row in m:
+        print(row)
+
+
 def mymain():
     matrix_subtraction()
 
